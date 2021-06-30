d9124e0a129053a7009b9c94be625e32ca6acc4b
imronshoimov <imronshoimov125@gmail.com>
2021-07-01 02:43:58 +0500

    Fetch and render videos

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,4 +1,5 @@
 const userList = document.querySelector('.user-list')
+const vidoeList = document.querySelector('.vidoe-list')
 const uploadBtn = document.querySelector('.upload-btn')
 
 async function renderUsers () {
@@ -14,6 +15,60 @@ async function renderUsers () {
 
         avatar.setAttribute('src', user.image)
         name.textContent = user.username
+
+        li.addEventListener('click', async event => {
+            vidoeList.textContent = ""  
+            let videos = await request('/videos', 'GET')
+            for(let item of videos) {
+                if (user.userId === item.userId) {
+                    let li = document.createElement('li')
+                    let video = document.createElement('video')
+                    let source = document.createElement('source')
+                    let videosInfo = document.createElement('div')
+                    let videoInfo = document.createElement('div')
+                    let videoAvatar = document.createElement('img')
+                    let span = document.createElement('span')
+                    let videoUser = document.createElement('p')
+                    let videoName = document.createElement('p')
+                    let button = document.createElement('button')
+                    let img = document.createElement('img')
+
+                    li.classList.add('video-item')
+                    video.classList.add('upload-video')
+                    video.setAttribute('controls', true)
+                    video.setAttribute('width', "450px")
+                    video.setAttribute('height', "250px")
+                    source.setAttribute('src', item.video)
+                    videosInfo.classList.add('videos-info')
+                    videoInfo.classList.add('vidoe-info')
+                    videoAvatar.classList.add('video-avatar')
+                    videoAvatar.src = user.image
+                    videoUser.classList.add('video-user')
+                    videoName.classList.add('video-name')
+                    button.classList.add('down-button')
+                    button.type = "button"
+                    img.classList.add('down-image')
+                    img.src = "/images/download.png"
+                    img.alt = "download"
+
+                    videoUser.textContent = user.username
+                    videoName.textContent = item.vide_name
+                    console.log(item.video);
+
+                    video.appendChild(source)
+                    span.appendChild(videoUser)
+                    span.appendChild(videoName)
+                    videoInfo.appendChild(videoAvatar)
+                    videoInfo.appendChild(span)
+                    videosInfo.appendChild(videoInfo)
+                    button.appendChild(img)
+                    videosInfo.appendChild(button)
+                    li.appendChild(video)
+                    li.appendChild(videosInfo)
+                    vidoeList.appendChild(li)
+                }
+            }
+        })
 
         li.appendChild(avatar)
         li.appendChild(name)
