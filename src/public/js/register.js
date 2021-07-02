a7d38b538a2cc5e503068386f7b378b5f235a672
imronshoimov <imronshoimov125@gmail.com>
2021-07-02 10:32:40 +0500

    changes

--- src/public/js/register.js
+++ src/public/js/register.js
@@ -13,7 +13,7 @@ form.onsubmit = async (event) => {
         
     })
     response = await response.json()
-    if(response) {
+    if(response.message) {
         display.textContent = response.message
         setTimeout(() => {
             window.location = '/upload'
