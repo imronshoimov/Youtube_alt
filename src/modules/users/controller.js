86a5bb65ff7579c4b46dcf191bc3d781534c9569
imronshoimov <imronshoimov125@gmail.com>
2021-07-02 11:56:07 +0500

    changes

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const GET = (req, res) => {
-    let videos = fs.readFileSync(path.join(process.cwd(), 'src', 'database', 'users.json'))
-    return res.send(videos)
+    let users = fs.readFileSync(path.join(process.cwd(), 'src', 'database', 'users.json'))
+    return res.send(users)
 }
 
 module.exports = { GET }
